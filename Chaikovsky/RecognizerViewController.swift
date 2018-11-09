d36bc24baf0533baed651021107f7171a126c0ae
Alexey Salangin <a7ls@ya.ru>
2018-11-09 19:05:29 +0300

    Rakgmaninov recognition

--- Chaikovsky/RecognizerViewController.swift
+++ Chaikovsky/RecognizerViewController.swift
@@ -10,66 +10,128 @@ import UIKit
 import SceneKit
 import ARKit
 
-class RecognizerViewController: UIViewController, ARSCNViewDelegate {
+class RecognizerViewController: UIViewController {
 
-    @IBOutlet var sceneView: ARSCNView!
-    
+    // MARK: - Properties
+
+    @IBOutlet private var sceneView: ARSCNView!
+
+    private var imageConfiguration: ARImageTrackingConfiguration?
+    private var hasSetWorldOrigin = false
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        //sceneView.scene = scene
+        view.isUserInteractionEnabled = true
+        setupSceneView()
+        setupImageDetection()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
+        if let configuration = imageConfiguration {
+            sceneView.debugOptions = .showFeaturePoints
+            sceneView.session.run(configuration)
+        }
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    // MARK: - Private methods
+
+    private func setupSceneView() {
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        let scene = SCNScene()
+        sceneView.scene = scene
     }
-*/
-    
+
+    private func setupImageDetection() {
+        imageConfiguration = ARImageTrackingConfiguration()
+
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Images",bundle: nil) else {
+                fatalError("Missing expected asset catalog resources.")
+        }
+        imageConfiguration?.trackingImages = referenceImages
+    }
+
+}
+
+extension RecognizerViewController: ARSessionDelegate {
+
+    // MARK: - ARSessionDelegate
+
     func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+        guard
+            let error = error as? ARError,
+            let code = ARError.Code(rawValue: error.errorCode)
+            else { return }
+
+        switch code {
+        case .cameraUnauthorized:
+            print("Camera tracking is not available. Please check your camera permissions.")
+        default:
+            print("Error starting ARKit. Please fix the app and relaunch.")
+        }
     }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        switch camera.trackingState {
+        case .limited(let reason):
+            switch reason {
+            case .excessiveMotion:
+                print("Too much motion! Slow down.")
+            case .initializing, .relocalizing:
+                print("ARKit is doing it's thing. Move around slowly for a bit while it warms up.")
+            case .insufficientFeatures:
+                print("Not enough features detected, try moving around a bit more or turning on the lights.")
+            }
+        case .normal:
+            print("Point the camera at a portrait.")
+        case .notAvailable:
+            print("Camera tracking is not available.")
+        }
     }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+
+}
+
+extension RecognizerViewController: ARSCNViewDelegate {
+
+    // MARK: - ARSCNViewDelegate
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let imageAnchor = anchor as? ARImageAnchor {
+            handleFoundImage(imageAnchor, node)
+        }
     }
+
+    private func handleFoundImage(_ imageAnchor: ARImageAnchor, _ node: SCNNode) {
+        DispatchQueue.main.async {
+            guard let name = imageAnchor.referenceImage.name else { return }
+            print("You found a \(name) image")
+
+            if !self.hasSetWorldOrigin {
+                self.sceneView.session.setWorldOrigin(relativeTransform: imageAnchor.transform)
+                self.hasSetWorldOrigin = true
+
+                let size = imageAnchor.referenceImage.physicalSize
+
+                let plane = SCNPlane(width: size.width, height: size.height)
+                let image = UIImage(named: "\(name)-origin")
+                let imageNode = SCNNode(geometry: plane)
+                imageNode.geometry?.firstMaterial?.diffuse.contents = image
+                imageNode.name = name
+                imageNode.opacity = 0.3
+                imageNode.eulerAngles.x = -.pi / 2
+
+                node.addChildNode(imageNode)
+            }
+
+        }
+    }
+
 }
