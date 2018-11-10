0e2c17501d45d5dd9801651d640e0ece12289277
Alexey Salangin <a7ls@ya.ru>
2018-11-10 16:50:42 +0300

    Speech recognition and synthesis

--- Chaikovsky/ViewControllers/SiriViewController.swift
+++ Chaikovsky/ViewControllers/SiriViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import SwiftSiriWaveformView
+import Speech
+import AVFoundation
 
 class SiriViewController: UIViewController {
 
@@ -16,10 +18,23 @@ class SiriViewController: UIViewController {
     private var change: CGFloat = 0.01
     private var hue: CGFloat = 0.0
 
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer(locale: Locale.init(identifier: "ru-RU"))
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         waveformView.amplitude = 1.0
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(refreshAudioView(_:)), userInfo: nil, repeats: true)
+
+        let string = "Смотрите как я могу говорить, ыыыыыыыыы "
+        //speak(text: string)
+
+
+        requestSpeechAuthorization()
+        recognizeSpeech()
+        //isRecording = true
     }
 
     @objc
@@ -33,7 +48,84 @@ class SiriViewController: UIViewController {
         if hue >= 1.0 { hue = 0.0 }
     }
 
+    @IBAction func start(_ sender: Any) {
 
+    }
+
+    private func speak(text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "ru-RU")
+
+        let synth = AVSpeechSynthesizer()
+        synth.speak(utterance)
+    }
+
+    private func recognizeSpeech() {
+        let node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+            self.request.append(buffer)
+        }
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        guard let myRecognizer = SFSpeechRecognizer() else {
+            print("Speech recognition is not supported for your current locale.")
+            return
+        }
+        if !myRecognizer.isAvailable {
+            print("Speech recognition is not currently available. Check back at a later time.")
+            return
+        }
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
+            if let result = result {
+
+                let bestString = result.bestTranscription.formattedString
+                print(bestString)
+
+                var lastString: String = ""
+                for segment in result.bestTranscription.segments {
+                    let indexTo = bestString.index(bestString.startIndex, offsetBy: segment.substringRange.location)
+                    lastString = String(bestString[indexTo...])
+                }
+                self.checkForColorsSaid(resultString: lastString)
+            } else if let error = error {
+                print("There has been a speech recognition error.")
+                print(error)
+            }
+        })
+    }
+
+    func requestSpeechAuthorization() {
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            print(authStatus)
+        }
+    }
+
+    func checkForColorsSaid(resultString: String) {
+        switch resultString {
+        case "красный":
+            view.backgroundColor = UIColor.red
+        case "оранжевый":
+            view.backgroundColor = UIColor.orange
+        case "жёлтый":
+            view.backgroundColor = UIColor.yellow
+        case "зелёный":
+            view.backgroundColor = UIColor.green
+        case "синий":
+            view.backgroundColor = UIColor.blue
+        case "чёрный":
+            view.backgroundColor = UIColor.black
+        case "белый":
+            view.backgroundColor = UIColor.white
+        case "серый":
+            view.backgroundColor = UIColor.gray
+        default: break
+        }
+    }
 
     /*
     // MARK: - Navigation
@@ -44,5 +136,11 @@ class SiriViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+}
+
+extension SiriViewController: SFSpeechRecognizerDelegate {
+
+    
 
 }
